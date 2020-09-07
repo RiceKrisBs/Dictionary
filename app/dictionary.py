1d00428d031c4ded035992dcc158c7389332158d
Kris Bucyk <krisbucyk@gmail.com>
2020-09-07 08:38:52 -0400

    Update logic to find proper nouns in dictionary

--- app/dictionary.py
+++ app/dictionary.py
@@ -13,7 +13,7 @@ def get_user_word() -> str:
 
 
 def get_word_definitions(word: str) -> list:
-    return dictionary_data.get(word)
+    return dictionary_data.get(word) or dictionary_data.get(word.title())
 
 
 def print_definitions(definitions: list) -> NoReturn:
